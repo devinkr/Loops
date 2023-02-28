04ca8dda6bdf5632869a0f709ee17edc157dddc9
Devin Raleigh <devinkr@gmail.com>
2023-02-28 14:40:33 -0700

    Is Even Number Challenge second part.

--- src/isEvenNumber.java
+++ src/isEvenNumber.java
@@ -1,14 +1,23 @@
 public class isEvenNumber {
 
     public static void main(String[] args) {
-        int i = 5;
+        int evenTotal = 0;
+        int oddTotal = 0;
+        int i = 4;
         while (i <= 20) {
+            i++;
+
             if (isEven(i)) {
                 System.out.println(i + " is an even number");
+                evenTotal++;
+            } else {
+                oddTotal++;
             }
-            i++;
+            if (evenTotal >= 5) {
+                break;
+            }
         }
-
+        System.out.println("Total Even: " + evenTotal + " Total Odd: " + oddTotal);
     }
 
     public static boolean isEven(int number) {
